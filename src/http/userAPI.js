0fbed626c741cb17ddc424ed032899aaf6b616c5
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-05-27 02:10:12 +0300

    remove console

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -1,21 +1,19 @@
 import { $host } from '.';
 
 export const authByPassword = async () => {
-  if (!localStorage.getItem('access')) {
-    const { data } = await $host.get('/2.0/oauth2/password', {
-      params: {
-        login: 'sergei.stralenia@gmail.com',
-        password: 'paralect123',
-        client_id: 2356,
-        client_secret:
-          'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
-        hr: 0,
-      },
-    });
+  const { data } = await $host.get('/2.0/oauth2/password', {
+    params: {
+      login: 'sergei.stralenia@gmail.com',
+      password: 'paralect123',
+      client_id: 2356,
+      client_secret:
+        'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
+      hr: 0,
+    },
+  });
 
-    localStorage.setItem('access', data.access_token);
-    localStorage.setItem('refresh', data.refresh_token);
+  localStorage.setItem('access', data.access_token);
+  localStorage.setItem('refresh', data.refresh_token);
 
-    return data;
-  }
+  return data;
 };
